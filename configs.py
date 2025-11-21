c88969b68538b133b318ef4b6391869877443aa9
Narek Gabrielyan <narekgabrielyan20@gmail.com>
2025-11-21 19:33:40 +0400

    fix railway build

--- configs.py
+++ configs.py
@@ -1,5 +1,2 @@
 mail_sender = "narekgabrielyan20@gmail.com"
 mail_receiver = "narek2888@mail.ru"
-sendgrid_railway_key = "SPB4KLEXFYYVHHXP24M8UPYD"
-sendgrid_api_key = "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"
-
